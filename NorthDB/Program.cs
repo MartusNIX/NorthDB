d3d75081039b8d0552a990ec321f08d2baabd47e
Daria Martus <martusdarja@gmail.com>
2022-11-03 10:55:03 +0200

    Add project files.

---
+++ NorthDB/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace NorthDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            using (NorthwindContext db = new NorthwindContext())
+            {
+                var categories = db.Categories.ToList();
+                Console.WriteLine("Current data:");
+                foreach (Category c in categories)
+                {
+                    Console.WriteLine($"{c.CategoryId}.{c.CategoryName}.{c.Description}");
+                }
+            }
+        }
+    }
+}
